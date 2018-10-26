e218c584ee97deb1634572025e24da4d6f6c1fc6
Nick James <nick.james@minted.com>
2018-10-26 12:04:26 -0700

    aliases + tmux scroll

---
+++ alias.sh
@@ -0,0 +1,9 @@
+alias bashrc="vim ~/.bash_profile"
+alias bsrc="source ~/.bash_profile"
+alias zsrc="source ~/.zshrc"
+alias tmconf="vim ~/.tmux.conf"
+alias tma="tmux attach -t"
+alias tml="tmux ls"
+alias tmrs="tmux rename-session"
+alias tmrw="tmux rename-window"
+
